9185dd0c88028afc44a78a86bb5a14a703c879d5
Rick Fujiyama <richard_fujiyama@hotmail.com>
2014-08-22 15:08:08 -0700

    [ETHERNET] fix comparison against ETHERTYPEs by defining endian-dependent versions

--- ethernet.c
+++ ethernet.c
@@ -1,1 +1,30 @@
 #include "ethernet.h"
+
+int ethernet_is_valid (const struct ethernet_pkt *pkt, 
+					   const struct ether_addr   *me) {
+
+	if ((pkt->h.ether_type != IP4_ETHERTYPE) && 
+		(pkt->h.ether_type != ARP_ETHERTYPE))
+		return 0;
+
+	if (!ethernet_addr_equals(me, (struct ether_addr*)(pkt->h.ether_dhost)) && 
+		!ethernet_addr_is_broadcast((struct ether_addr*)(pkt->h.ether_dhost)))
+		return 0;
+	return 1;
+}
+
+int ethernet_addr_is_broadcast (const struct ether_addr *addr) {
+	uint64_t broadcast = UINT64_MAX;
+	if (memcmp(addr, &broadcast, ETHER_ADDR_LEN) == 0)
+		return 1;
+	else
+		return 0;
+}
+
+int ethernet_addr_equals(const struct ether_addr *a, 
+						const struct ether_addr *b) {
+	if (memcmp(a, b, ETHER_ADDR_LEN) == 0)
+		return 1;
+	else
+		return 0;
+}
--- ethernet.h
+++ ethernet.h
@@ -1,2 +1,35 @@
-#include <sys/types.h>  // u_char, required for <net/ethernet.h>
-#include <net/ethernet.h> //ether_header, ether_ntoa_r
+#ifndef _ETHERNET_
+#define _ETHERNET_
+
+#include <sys/types.h>  	// u_char, required for <net/ethernet.h>
+#include <net/ethernet.h> 	// ether_header, ether_ntoa_r
+#include <stdint.h>			// UINT64_MAX
+#include <string.h>			// memcmp
+
+
+#if BYTE_ORDER == LITTLE_ENDIAN
+#define ARP_ETHERTYPE		0x0608
+#define IP4_ETHERTYPE		0x0008
+#define IP6_ETHERTYPE		0xDD86
+#endif
+#if BYTE_ORDER == BIG_ENDIAN
+#define ARP_ETHERTYPE		ETHERTYPE_ARP
+#define IP4_ETHERTYPE		ETHERTYPE_IP
+#define IP6_ETHERTYPE		ETHERTYPE_IPV6
+#endif
+
+
+// technically a frame, but for the sake of consistency
+struct ethernet_pkt {
+	struct ether_header h;
+	//unsigned char data[];
+} __attribute__((__packed__));
+
+
+int ethernet_is_valid (const struct ethernet_pkt *pkt,
+                       const struct ether_addr   *me);
+int ethernet_addr_is_broadcast (const struct ether_addr *addr);
+int ethernet_addr_equals(const struct ether_addr *a,
+                        const struct ether_addr *b);
+
+#endif
