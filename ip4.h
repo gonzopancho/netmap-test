e8b3c10d23473d48b68f3135fea718c5ed117bb3
Rick Fujiyama <richard_fujiyama@hotmail.com>
2014-08-22 20:37:41 -0700

    [IP4] add print function

--- ip4.h
+++ ip4.h
@@ -5,13 +5,18 @@
 #include <netinet/in.h> // IP protos, required for <netinet/ip.h>
 #include <netinet/ip.h> // ip
 #include <arpa/inet.h>	// inet_aton_r
+#include <stdio.h>	// printf
+#include <inttypes.h>	// PRI macros
+
+
 
 /* assumes no VLAN tags or IP options */
 struct ip4_pkt {
-	struct ether_header ether_h;
 	struct ip ip_h;
+	unsigned char data[];
 } __attribute__((__packed__));
 
 int ip4_is_valid(struct ip4_pkt *ip);
+void ip4_print(struct ip4_pkt *ip);
 
 #endif
