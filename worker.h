45f437c06591d391bdb64d8ed66b79ddc8a0fe35
Richard Fujiyama <richard_fujiyama@hotmail.com>
2014-11-30 11:38:57 -0800

    [WORKER] remove code from old single threaded version

--- worker.h
+++ worker.h
@@ -9,34 +9,6 @@
 #include <unistd.h>     // sleep
 #include "common.h"
 
-#if 0
-struct receive_queue {
-  int num_entries;
-  int max_entries;
-  int *data;
-};
-
-
-struct worker_data {
-  int thread_id;
-  int num_receive_queues;
-  int current_receive_queue;
-  struct receive_queue *receive_queues;
-  pthread_mutex_t receive_queue_ready_lock;
-  pthread_cond_t receive_queue_ready;
-};
-
-int next_receive_queue(struct worker_data *d);
-int receive_queue_insert(struct receive_queue *q, int data);
-int initialize_receive_queue(struct receive_queue *queue,
-                              int max_receive_entries);
-int initialize_worker_data(struct worker_data *data,
-                            int num_receive_queues,
-                            int max_receive_entries);
-int worker_data_next_receive_queue(struct worker_data *d);
-void *worker(void *threadarg);
-#endif
-
 
 struct worker_data {
   int msg_q_capacity;
